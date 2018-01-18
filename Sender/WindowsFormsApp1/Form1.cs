a6b870a34322d044e85f7833305bd21e5e5a5dd3
amirsoleimanii <35392182+amirsoleimanii@users.noreply.github.com>
2018-01-18 18:39:05 +0330

    Add files via upload
    این فرم برای ارسال یک رشته ی بیتی از طریق پروتکل UDP نوشته طراحی شده است.

---
+++ Sender/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sender
+{
+    public partial class Form1 : Form
+    {
+        string pm = "11101100";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pm = txtbHost.Text;
+            UdpClient udpClient = new UdpClient();
+            udpClient.DontFragment = true;
+            udpClient.Connect(txtbHost.Text, 8080);
+            Byte[] senddata = Encoding.ASCII.GetBytes("Hello World");
+
+            for (int i = 0; i < pm.Length; i++)
+            {
+                if (pm[i] == '1')
+                {
+                    udpClient.Send(senddata, senddata.Length);
+                    udpClient.Send(senddata, senddata.Length);
+                }
+                else
+                {
+                    udpClient.Send(senddata, senddata.Length);
+                    Thread.Sleep(15000);
+                    udpClient.Send(senddata, senddata.Length);
+                }
+            }
+
+          
+        }
+    }
+}
